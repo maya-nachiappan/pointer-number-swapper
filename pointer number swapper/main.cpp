1a8432f6834a1e366f2db3ce7753e55ff4845e99
maya-nachiappan <maya.n102304@gmail.com>
2024-10-08 21:41:40 -0700

    created function to swap to variables provided by the user by using pointers

--- pointer number swapper/main.cpp
+++ pointer number swapper/main.cpp
@@ -4,11 +4,27 @@
 //
 //  Created by maya nachiappan on 10/8/24.
 //
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+// Function to swap two variables using pointers
+void swap(int* a, int* b) {
+    int temp = *a; // Store the value at the address pointed by a
+    *a = *b;       // Assign the value at the address pointed by b to the address pointed by a
+    *b = temp;     // Assign the value stored in temp to the address pointed by b
+}
+
+int main() {
+    int x, y;
+
+   
+    cout << "Enter two integers: "<< endl;
+    cin >> x >> y;
+
+    cout << "Before swap: x = " << x << ", y = " << y << endl;
+    swap(&x, &y);
+
+    cout << "After swap: x = " << x << ", y = " << y <<endl;
+
     return 0;
 }
