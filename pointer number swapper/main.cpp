a772f434898239def04ec277709c15e0cf0a3e43
maya-nachiappan <maya.n102304@gmail.com>
2024-10-28 18:55:47 -0700

    fixed in 1 file

--- pointer number swapper/main.cpp
+++ pointer number swapper/main.cpp
@@ -5,13 +5,28 @@
 //  Created by maya nachiappan on 10/8/24.
 //
 #include <iostream>
-#include "swap.hpp"
+using namespace std;
 
-
-int main() 
-{
-    print();
-    swap();
-    return 0;
+    
+// Function to swap two variables using pointers
+void swap(int* a, int* b) {
+    int temp=*a;// assigns the value at the address pointed by a
+    *a = *b; // assignts the balue at the address pointed by b to the address pointed by a
+    *b = temp; // assigns the value stored in temp to address b
 }
+int main ()
+{
+
+        
+        int x, y;
+        
+        cout << "Enter two integers: "<< endl;
+        cin >> x >> y; // stores the values of x and y
+        
+        cout << "Before swap: x = " << x << ", y = " << y << endl; // prints values of x and y before swapping
+        
+        swap(&x, &y); // uses swap function to swap values based on address of x and y
+        
+        cout << "After swap: x = " << x << ", y = " << y <<endl; // prints swapped values of x and y
+    }
 
